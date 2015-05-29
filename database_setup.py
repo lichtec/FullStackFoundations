a93cc82ef404934ba1d0bc2b5290ba21446435e3
Chris L <chris.lichter@gmail.com>
2015-05-28 22:41:17 -0500

    added to config

--- database_setup.py
+++ database_setup.py
@@ -6,3 +6,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import relationship
 
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+#######insert at end of file############
+
+engine = create_engine(
+	'sqlite:////restaurantmenu.db')
+
